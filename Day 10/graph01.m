73139098e441ffa8a69b95c44716f0504ad7bc67
Ms. Fathima Hima M.F. <fathimahima2020@gmail.com>
2025-06-14 18:16:02 +0530

    Add files via upload

---
+++ Day 10/graph01.m
@@ -0,0 +1,5 @@
+s=[1 2];%starting node
+t=[2 3];%ending node
+G=graph(s, t); %create graph Object
+plot(G);  %Plot the graph
+title('Graph'); %add a title
---
+++ Day 10/graph02.m
@@ -0,0 +1,4 @@
+G = graph([1 2],[2 3]);
+plot(G);
+title('Undirected Graph');
+
