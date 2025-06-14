7786db4a6ee028cc6bcfcc189dddfb7ad9d68e87
Ms. Fathima Hima M.F. <fathimahima2020@gmail.com>
2025-06-14 17:53:40 +0530

    Add files via upload

---
+++ Day 02/userinput.m
@@ -0,0 +1,21 @@
+% get user input for integers
+x=input("Enter the number ");
+disp(['you entered ',num2str(x)])
+
+% get user input for string
+name=input("Enter your name ",'s');
+disp("You entered "+name);
+
+% get user input for integers
+age=input('Enter your age ');
+disp(['you entered ',num2str(x)])
+
+% get user input for string
+name=input('Enter your first name ','s');
+disp(['You entered ',name]);
+
+n=1;
+while n<10
+    disp(n)
+    n=n+1;
+end
