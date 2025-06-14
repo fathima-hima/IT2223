9c9210ae154ac7ea4de6a9429274bc6805e6e29a
Ms. Fathima Hima M.F. <fathimahima2020@gmail.com>
2025-06-14 21:43:54 +0530

    Add files via upload

---
+++ Day 01/day1.m
@@ -0,0 +1,21 @@
+num=4;
+
+if num>0
+    disp('The num is positive')
+else
+    disp('The number is negative')
+end
+
+
+number=5;
+if mod(number,2)==0
+    disp('The number is even')
+else
+    disp('The number is odd')
+end
+
+n=1;
+while n<=5
+    disp(n)
+    n=n+1;
+end
