a817a0ca491baa3aff0de26a8f5fa3bf0de09b11
Ms. Fathima Hima M.F. <fathimahima2020@gmail.com>
2025-06-17 13:09:29 +0530

    Add files via upload

---
+++ Day 12/tree2.m
@@ -0,0 +1,14 @@
+nodes={'A','B','C','D','E','F','G'};
+
+%define edges using source and target nodes
+s=[1 1 2 2 3 3]; %from A->B, A->C,, B->D, B->E, c
+t=[2 3 4 5 6 7]; %To
+
+%define edges with weights
+weights=[5 3 2 4 6 1];
+%created directed graph
+G=digraph(s,t,weights,nodes);
+
+%plot the tree with edge weights displayed
+plot(G,'Layout','layered','Direction','Down','EdgeLabel',G.Edges.Weight);
+title('Weighted tree with 3 levels');
